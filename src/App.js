a1535e814d954149eb2c92c50e18418b34393618
João Victor <joao.victor.valentim@hotmail.com>
2023-07-17 10:18:44 -0300

    style: add esqueleto do projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { GrFormPreviousLink, GrFormNextLink } from 'react-icons/gr';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Formulário de Inscrição</h2>
+        <small>Formulário de Estudo: Nenhuma informação será salva ou utilizada.</small>
+        <p>Obrigado por completar o formulário! Entraremos em contato em breve.</p>
+      </div>
+      <div className='form-container'>
+        <p>etapas</p>
+        <form>
+          <div className='inputs-container'></div>
+          <div className='action'></div>
+          <button type='button'>
+            <GrFormPreviousLink />
+            <span>Voltar</span>
+          </button>
+          <button type='submit'>
+            <span>Avançar</span>
+            <GrFormNextLink />
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
